6be90adb24f2f3db0de34cbf31e3df7c3bab95d4
Megaf <mmegaf@gmail.com>
2014-12-02 17:08:56 +0000

    Cleaned up the code a bit.

--- init.lua
+++ init.lua
@@ -1,26 +1,25 @@
 local pixels = {}
 pixels.colors = {
-	{"white",      "White",      nil},
-	{"grey",       "Grey",       "basecolor_grey"},
-	{"black",      "Black",      "basecolor_black"},
-	{"red",        "Red",        "basecolor_red"},
-	{"yellow",     "Yellow",     "basecolor_yellow"},
-	{"green",      "Green",      "basecolor_green"},
-	{"cyan",       "Cyan",       "basecolor_cyan"},
-	{"blue",       "Blue",       "basecolor_blue"},
-	{"magenta",    "Magenta",    "basecolor_magenta"},
-	{"orange",     "Orange",     "excolor_orange"},
-	{"violet",     "Violet",     "excolor_violet"},
-	{"brown",      "Brown",      "unicolor_dark_orange"},
-	{"pink",       "Pink",       "unicolor_light_red"},
-	{"dark_grey",  "Dark Grey",  "unicolor_darkgrey"},
-	{"dark_green", "Dark Green", "unicolor_dark_green"},
+	{"white",      "White"},
+	{"grey",       "Grey"},
+	{"black",      "Black"},
+	{"red",        "Red"},
+	{"yellow",     "Yellow"},
+	{"green",      "Green"},
+	{"cyan",       "Cyan"},
+	{"blue",       "Blue"},
+	{"magenta",    "Magenta"},
+	{"orange",     "Orange"},
+	{"violet",     "Violet"},
+	{"brown",      "Brown"},
+	{"pink",       "Pink"},
+	{"dark_grey",  "Dark Grey"},
+	{"dark_green", "Dark Green"},
 }
 
 for _, row in ipairs(pixels.colors) do
 	local name = row[1]
 	local desc = row[2]
-	local craft_color_group = row[3]
 	minetest.register_node("pixels:"..name, {
 		description = desc.." Pixel",
 		tiles = {"pixel_"..name..".png"},
@@ -33,7 +32,6 @@ end
 for _, row in ipairs(pixels.colors) do
 	local name = row[1]
 	local desc = row[2]
-	local craft_color_group = row[3]
 	minetest.register_node("pixels:"..name.."_framed", {
 		description = "Framed (black border)"..desc.." Pixel",
 		tiles = {"pixel_"..name..".png^pixel_frame.png"},
